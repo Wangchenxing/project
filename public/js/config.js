2ce430c965dfe7d999171e218b5b579913e702b3
wangcx <mr_wangcx@163.com>
2017-06-11 16:17:35 +0800

    讲师相关功能完结

--- public/js/config.js
+++ public/js/config.js
@@ -4,8 +4,11 @@ requirejs.config({
         jquery: 'jquery/jquery.min',
         bootstrap: 'bootstrap/js/bootstrap.min',
         template: 'artTemplate/template-web',
+        validate:'validate/jquery-validate',
         cookie: 'jquery-cookie/jquery.cookie',
         nprogress:'nprogress/nprogress',
+        datepicker:'bootstrap-datepicker/js/bootstrap-datepicker.min',
+        language:'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
         common: '../js/common',
         util: '../js/util',
         // echarts:'echarts/echarts.min',
@@ -17,6 +20,15 @@ requirejs.config({
     shim: { // 兼容非标准模块
         bootstrap: {
             deps: ['jquery']
+        },
+        datepicker: {
+            deps: ['jquery']
+        },
+        language: {
+            deps: ['jquery','datepicker']
+        },
+        validate:{
+            deps:['jquery']
         }
     }
 });
--- public/js/teacher_add.js
+++ public/js/teacher_add.js
@@ -1,38 +1,99 @@
-define(['jquery','template'],function($,template){
-	if(location.search){
-		// var index =  location.search.indexOf("=");
-		var tcId =  location.search.split("=")[1];
+define(['jquery', 'template', 'datepicker', 'language', 'validate'], function($, template) {
+
+	$('.datepicker').datepicker({
+		endDate: '0d'
+	});
+	if (location.search) {
+		$('.breadcrumb .active').html('讲师编辑');
+		var tcId = location.search.split("=")[1];
 		$.ajax({
 			url: '/api/teacher/edit',
 			type: 'GET',
 			dataType: 'json',
-			data: {tc_id:tcId},
-			success:function(data){
-				var html = template('teacherAddTpl',data.result);
+			data: {
+				tc_id: tcId
+			},
+			success: function(data) {
+				var html = template('teacherAddTpl', data.result);
 				$("#teacherAddinfo").html(html);
-				 editTeacherFun();
+				editTeacherFun();
 			}
 		});
-		
-	}else {
-		console.log('1111');
+
+	} else {
+		addTeacherFun();
 	}
 
-	function  editTeacherFun(){
+	function editTeacherFun() {
 		$("#addTeacherBtn").click(function(event) {
 			$.ajax({
 				url: '/api/teacher/update',
 				type: 'post',
 				dataType: 'json',
-				data: {tc_id:tcId},
-				success:function(data){
+				data: $("#teacherAddinfo").serialize(),
+				success: function(data) {
 					if (data.code == 200) {
 						location.href = '/teacher/teacher_list';
 					}
 				}
 			});
-			
+
 		});
 	}
-	
+
+	function addTeacherFun() {
+		$("#teacherAddinfo").validate({
+			sendForm: false,
+			eachInvalidField: function() {
+				console.log(1);
+			},
+			eachValidField: function() {
+				console.log(2);
+			},
+			valid: function() {
+				$.ajax({
+					url: '/api/teacher/add',
+					type: 'post',
+					dataType: 'json',
+					data: $("#teacherAddinfo").serialize(),
+					success: function(data) {
+						if (data.code == 200) {
+							location.href = '/teacher/teacher_list';
+						}
+					}
+				});
+				console.log(3);
+			},
+			description:{
+				tcName:{
+					required:'姓名不能为空',
+					valid:'姓名可以使用'
+				},
+				tcPass:{
+					required:'密码不能为空',
+					pattern:'密码必须是6位数字',
+					valid:'姓名可以使用'
+				},
+				tcJoinDate:{
+					required:'日期不能为空',
+					valid:'日期格式正确'
+				}
+			}
+		});
+		// $("#addTeacherBtn").click(function(event) {
+		// 	// $.ajax({
+		// 	// 	url: '/api/teacher/add',
+		// 	// 	type: 'post',
+		// 	// 	dataType: 'json',
+		// 	// 	data: $("#teacherAddinfo").serialize(),
+		// 	// 	success:function(data){
+		// 	// 		if (data.code == 200) {
+		// 	// 			location.href = '/teacher/teacher_list';
+		// 	// 		}
+		// 	// 	}
+		// 	// });
+
+		// });
+	}
+
 });
